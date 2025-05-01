6fc548089c78eb2143fc21ab2860ce15cc84fa95
Dimitar <dimi.dimitrow27@gmail.com>
2025-05-01 17:16:58 +0200

    integrate SQLiteDataManager into Flask app

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+import os
 from flask import Flask
+from datamanager.sqlite_data_manager import SQLiteDataManager
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+os.makedirs(app.instance_path, exist_ok=True)
 
-@app.route('/')
-def home():
-    return "Welcome to MovieWeb App!"
+db_path = os.path.join(app.instance_path, "moviwebapp.db")
+data_manager = SQLiteDataManager(app, db_path)
 
 
+@app.route('/users')
+def list_users():
+    users = data_manager.get_all_users()
+    return str(users)  # Temporarily returning users as a string
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
